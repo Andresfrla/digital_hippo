0ee2772b1187b386373d507e2707b52d2cdb9a3b
Andres Franco <andresfranla@gmail.com>
2024-02-05 16:44:35 -0600

    Work on the logic fo sign in and sign up

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -13,12 +13,22 @@ import { AuthCredentialValidatior, TAuthCredentialsValidator } from "@/lib/valid
 import { trpc } from "@/trpc/client"
 import { toast } from "sonner"
 import { ZodError } from "zod"
-import { useRouter } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
 import { error } from "console"
 
 const Page = () => {
 
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const isSeller = searchParams.get('as') === 'seller'
   
+  const continueAsSeller = () => {
+    router.push("?as=seller")
+  }
+
+  const continueAsBuyer = () => {
+    router.replace('/sign-in', undefined)
+  }
 
   const { 
     register, 
@@ -28,35 +38,29 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialValidatior),
   })
 
-  const router = useRouter()
+  const { mutate: signIn, isLoading } =
+  trpc.auth.signIn.useMutation({
+    onSuccess: () => {
+      toast.success('Signed in successfully')
 
-  const { mutate, isLoading } =
-  trpc.auth.createPayloadUser.useMutation({
+      router.refresh()
+
+      if(origin) {
+        router.push(`/${origin}`)
+        return
+      }
+
+      if(isSeller) {
+        router.push('/sell')
+        return
+      }
+
+      router.push('/')
+    },
     onError: (err) => {
-      if (err.data?.code === 'CONFLICT') {
-        toast.error(
-          'This email is already in use. Sign in instead?'
-        )
-
-        return
-      }
-
-      if (err instanceof ZodError) {
-        toast.error(err.issues[0].message)
-
-        return
-      }
-
-      toast.error(
-        'Something went wrong. Please try again.'
-      )
-    },
-    onSuccess: ({ sentToEmail }) => {
-      toast.success(
-        `Verification email sent to ${sentToEmail}.`
-      )
-      router.push('/verify-email?to=' + sentToEmail)
-    },
+      if(err.data?.code === 'UNAUTHORIZED') {
+        toast.error("Invalid credentials")
+      }}
   })
 
   const onSubmit = ({
@@ -64,7 +68,7 @@ const Page = () => {
     password,
   }: TAuthCredentialsValidator) => {
     try {
-      mutate({ email, password });
+      signIn({ email, password });
     } catch (error) {
       console.error("Error during mutation:", error);
     }
@@ -77,14 +81,14 @@ const Page = () => {
           <div className="flex flex-col items-center space-y-2 text-center">
             <Icons.logo className="h-20 w-20"/>
             <h1 className="text-2xl font-bold">
-              Create an account
+              Sign in to your {isSeller ? "seller" : ""} account
             </h1>
 
             <Link className={buttonVariants({
               variant: 'link',
               className: 'gap-1.5'
-            })} href='/sign-in'>
-              Already have an account? Sign in
+            })} href='/sign-up'>
+              Don&apos;nt have and account?
               <ArrowRight className="size-4"/>
             </Link>
           </div>
@@ -126,9 +130,36 @@ const Page = () => {
                   </p>
                 )}
 
-                <Button>Sign up</Button>
+                <Button>Sign in</Button>
               </div>
             </form>
+
+            <div className="relative">
+                <div 
+                aria-hidden='true'
+                className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t"/>
+                </div>
+                <div className="realtive flex justify-center text-xs uppercase">
+                  <span className="bg-backgroun px-2 text-muted-foreground">
+                    or
+                  </span>
+                </div>
+            </div>
+
+            {isSeller ? (
+              <Button 
+              onClick={continueAsBuyer}
+              variant='secondary'
+              disabled={isLoading}
+              >Continue as custromer</Button>
+            ): (
+              <Button 
+              onClick={continueAsSeller}
+              variant='secondary'
+              disabled={isLoading}
+              >Continue as seller</Button>
+            )}
           </div>
         </div>
       </div>
