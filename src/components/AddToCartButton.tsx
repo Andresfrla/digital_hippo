5b1426544135e2254f0a9f407b8d1daf28d418b3
Andres Franco <andresfranla@gmail.com>
2024-02-07 15:32:42 -0600

    add the button for purchase

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { Button } from "./ui/button"
+
+const AddToCartButton = () => {
+    const [isSuccess, setIsSuccess] = useState<boolean>(false)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIsSuccess(false)
+        }, 2000)
+
+        return () => clearTimeout(timeout)
+    },[])
+
+  return (
+    <Button size='lg' className="w-full">Add To cart</Button>
+  )
+}
+
+export default AddToCartButton
