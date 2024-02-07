dddcd89b75a6b58b2ed44e4c7d8db88d56ab10ad
Andres Franco <andresfranla@gmail.com>
2024-02-06 21:03:52 -0600

    adding the products

---
+++ src/components/ProductListing.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { Product } from "@/payload-types"
+import { useState } from "react"
+import { Skeleton } from "./ui/skeleton"
+
+interface ProductListingProps {
+    product: Product | null
+    index: number
+}
+
+const ProductListing = ({product, index}: ProductListingProps) => {
+    const [isVisible, setIsVisible] = useState<boolean>(false)
+
+    if(!product || !isVisible) return <ProductPlaceHolder />
+}
+
+const ProductPlaceHolder = () => {
+    return (
+        <div className="flex flex-col w-full">
+            <div className="relative bg-zing-100 aspect-square w-full oferflow-hidden rounded-xl">
+                <Skeleton className="h-full w-full" />
+            </div>
+            <Skeleton className="mt-4 w-2/3 h-4 rounded-lg"/>
+            <Skeleton className="mt-2 w-16 h-4 rounded-lg"/>
+            <Skeleton className="mt-4 w-12 h-4 rounded-lg"/>
+        </div>
+    )
+}
+
+export default ProductListing
