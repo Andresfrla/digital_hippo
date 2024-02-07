aa52af0ce8a3876f670d7ed7c5e3e113473dd16e
Andres Franco <andresfranla@gmail.com>
2024-02-06 22:08:38 -0600

    config the state of the slides on the images

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,11 +1,34 @@
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
-
+import "swiper/css" 
+import "swiper/css/pagination"
+import { useEffect, useState } from 'react'
+import SwiperType from "swiper"
+import { Pagination } from 'swiper/modules'
 interface ImageSlidersProps {
     urls: string[]
 }
 
 function ImageSlider({urls}: ImageSlidersProps) {
+    const [swiper, setSwiper] = useState<null | SwiperType>(null)
+
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    const [slideConfig, setSlideConfig] = useState({
+        isBeginning: true,
+        isEnd: activeIndex === (urls.length ?? 0) - 1
+    })
+
+    useEffect(() => {
+        swiper?.on("slideChange", ({activeIndex}) => {
+            setActiveIndex(activeIndex)
+            setSlideConfig({
+                isBeginning: activeIndex === 0,
+                isEnd: activeIndex === (urls.length ?? 0) - 1
+            })
+        })
+    }, [swiper, urls])
+
     const activeStyles = "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white boder-zinc-300"
     const inactiveStyles = 'hidden text-gray-400'
   return (
@@ -15,7 +38,11 @@ function ImageSlider({urls}: ImageSlidersProps) {
             <button></button>
         </div>
 
-        <Swiper className='h-full w-full'>
+        <Swiper onSwiper={(swiper) => setSwiper(swiper)} 
+        spaceBetween={50}
+        modules={[Pagination]}
+        slidesPerView={1}
+        className='h-full w-full'>
     {urls.map((url, i) => (
         <SwiperSlide key={i} className='-z-10 relative h-full w-full'>
             <Image 
